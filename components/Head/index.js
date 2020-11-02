7243d2602aa7c7cf0e17ebe0c23a4596f4ca1892
Jan Rádl <radl.honza@gmail.com>
2020-11-02 08:08:07 +0100

    Changes

--- components/Head/index.js
+++ components/Head/index.js
@@ -48,17 +48,7 @@ const HeadComponent = (props) => (
       content="/static/favicon/browserconfig.xml"
     />
     <meta name="theme-color" content="#ffffff" />
-    <link
-      rel="stylesheet"
-      type="text/css"
-      charSet="UTF-8"
-      href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
-    />
-    <link
-      rel="stylesheet"
-      type="text/css"
-      href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
-    />
+    <link rel="stylesheet" href="https://cdn.plyr.io/3.5.2/plyr.css"></link>
   </Head>
 );
 
